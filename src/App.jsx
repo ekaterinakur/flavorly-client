9648cc98dcd58041f3a90fd74f9bb142420d056f
Rodion <rodion.groshev@gmail.com>
2025-04-29 13:29:28 +0200

    layout added and points for container

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
-import Footer from './components/Footer/Footer';
+import Layout from './components/Layout/Layout';
 import Home from './pages/Home';
 
 export default function App() {
   return (
-    <div className="app">
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Layout>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,6 @@ import Header from '../components/Header/Header';
 export default function Home() {
   return (
     <>
-      <Header />
-
       <main className="page-content">
         <div className="container main-container">
           <h1>Welcome to the Recipe App</h1>
