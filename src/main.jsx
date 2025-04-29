4a8c8adc468fb2d19820c0ea7abaf99b22c4fa5e
Dima <dima@vantage.software>
2025-04-29 18:56:10 +0300

    feat: added rtk and axios configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import App from './App';
 import './index.scss';
+import './config/axios.js';
+import { store } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
