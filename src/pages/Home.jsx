16035da51412fe9cd25b80e5a02d8f77bd834057
Rodion <rodion.groshev@gmail.com>
2025-04-29 17:45:23 +0200

    button component added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import Button from '../components/Button/Button';
+
 export default function Home() {
   return (
     <>
       <div className="container main-container">
         <h1>Welcome to the Recipe App</h1>
+        <Button size="small">SIGN IN</Button>
       </div>
     </>
   );
