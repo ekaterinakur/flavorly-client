258e109f58cb31487a23cec9becbede1062ad36f
ekaterinakur <ekaterina1.kur@gmail.com>
2025-05-01 20:35:15 +0300

    Fix import; remove mocks

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,10 @@
-import Button from '../components/Button/Button';
 import { RecipeList } from '../components/RecipeList/RecipeList';
-import Loader from '../components/Loader/Loader';
 import { RECIPES_LIST } from '../mocks/recipes-list';
 
 export default function HomePage() {
   return (
     <>
       <div className="container main-container">
-        <h1>Welcome to the Recipe App</h1>
-        <Button size="small">SIGN IN</Button>
-
         <RecipeList items={RECIPES_LIST} />
       </div>
     </>
