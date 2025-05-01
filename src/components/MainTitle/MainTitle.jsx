8fc9ab8ec47859ac69ad65ef658d34a76400f9a3
Denys <25481529+denys-log@users.noreply.github.com>
2025-05-01 09:27:57 +0300

    refactor: after review

--- src/components/MainTitle/MainTitle.jsx
+++ src/components/MainTitle/MainTitle.jsx
@@ -3,20 +3,11 @@ import styles from './MainTitle.module.scss';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-export function MainTitle({ tag: Tag = 'h1', className, children }) {
-  return (
-    <>
-      {React.createElement(
-        Tag,
-        { className: classNames(styles.title, className) },
-        children
-      )}
-    </>
-  );
+export function MainTitle({ className, children }) {
+  return <h1 className={classNames(styles.title, className)}>{children}</h1>;
 }
 
 MainTitle.propTypes = {
-  tag: PropTypes.string,
   className: PropTypes.string,
   children: PropTypes.node.isRequired,
 };
