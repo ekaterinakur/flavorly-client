38772d891d32d8ba9835ec1376c6469fc727372f
Rodion <rodion.groshev@gmail.com>
2025-05-02 14:17:41 +0200

    reworder container, and section

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -4,24 +4,26 @@ import styles from './RecipeList.module.scss';
 
 export function RecipeList({ items, columns = 3 }) {
   return (
-    <section aria-label="Recipes" className='section'>
-      <ul
-        className={classNames(styles.wrapper, {
-          [styles[`columns-${columns}`]]: columns,
-        })}
-      >
-        {items.map((recipe, idx) => (
-          <li key={idx}>
-            <RecipeCard
-              img={recipe.img}
-              title={recipe.title}
-              desc={recipe.desc}
-              authorName={recipe.authorName}
-              authorAvatar={recipe.authorAvatar}
-            />
-          </li>
-        ))}
-      </ul>
+    <section aria-label="Recipes" className="section">
+      <div className="container">
+        <ul
+          className={classNames(styles.wrapper, {
+            [styles[`columns-${columns}`]]: columns,
+          })}
+        >
+          {items.map((recipe, idx) => (
+            <li key={idx}>
+              <RecipeCard
+                img={recipe.img}
+                title={recipe.title}
+                desc={recipe.desc}
+                authorName={recipe.authorName}
+                authorAvatar={recipe.authorAvatar}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
