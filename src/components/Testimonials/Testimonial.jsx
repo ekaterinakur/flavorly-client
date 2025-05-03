3bc748cf6602ea210a6355f2aaf8a7b0eb766653
Dima <dima@vantage.software>
2025-05-03 12:35:00 +0300

    feat: connected api

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -2,8 +2,8 @@ const Testimonial = ({ testimonial }) => {
   return (
     <div key={testimonial.id} className="testimonial">
       <div className="testimonial-content">
-        <h3 className="testimonial-content-text">{testimonial.text}</h3>
-        <p className="testimonial-content-author">{testimonial.author}</p>
+        <h3 className="testimonial-content-text">{testimonial.testimonial}</h3>
+        <p className="testimonial-content-author">{testimonial.author ?? 'Fourth Change'}</p>
       </div>
     </div>
   );
