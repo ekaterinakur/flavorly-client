47a27c857a4ba5131ce919f5c0fbd4955c0cac37
Dima <dima@vantage.software>
2025-05-05 17:52:20 +0300

    chore: added testimonial author

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -3,7 +3,7 @@ const Testimonial = ({ testimonial }) => {
     <div key={testimonial.id} className="testimonial">
       <div className="testimonial-content">
         <h3 className="testimonial-content-text">{testimonial.testimonial}</h3>
-        <p className="testimonial-content-author">{testimonial.author ?? 'Fourth Change'}</p>
+        <p className="testimonial-content-author">{testimonial.owner?.name}</p>
       </div>
     </div>
   );
