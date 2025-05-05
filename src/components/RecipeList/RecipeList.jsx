812e3036277735a6112fa5064665521c23e95865
Denys <25481529+denys-log@users.noreply.github.com>
2025-05-05 21:38:14 +0300

    feat: add recipe page

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -4,26 +4,24 @@ import styles from './RecipeList.module.scss';
 
 export function RecipeList({ items, columns = 3 }) {
   return (
-    <section aria-label="Recipes" className="section">
-      <div className="container">
-        <ul
-          className={classNames(styles.wrapper, {
-            [styles[`columns-${columns}`]]: columns,
-          })}
-        >
-          {items.map((recipe, idx) => (
-            <li key={idx}>
-              <RecipeCard
-                img={recipe.img}
-                title={recipe.title}
-                desc={recipe.desc}
-                authorName={recipe.authorName}
-                authorAvatar={recipe.authorAvatar}
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
+    <section aria-label="Recipes">
+      <ul
+        className={classNames(styles.wrapper, {
+          [styles[`columns-${columns}`]]: columns,
+        })}
+      >
+        {items.map((recipe, idx) => (
+          <li key={idx}>
+            <RecipeCard
+              img={recipe.img}
+              title={recipe.title}
+              desc={recipe.desc}
+              authorName={recipe.authorName}
+              authorAvatar={recipe.authorAvatar}
+            />
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
