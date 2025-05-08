f3ebfdcd0f33f15bd5413ce9dcd829567d1992e0
Rodion <rodion.groshev@gmail.com>
2025-05-08 20:21:52 +0200

    added useState import

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import './Header.scss';
